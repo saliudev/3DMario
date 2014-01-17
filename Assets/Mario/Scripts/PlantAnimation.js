d3bd44d4efc2cbcad19d0376ce94abc894e674b0
liusashmily <liusashmily@gmail.com>
2014-01-17 17:30:03 -0600

    5 18 plant trigger animation

--- Assets/Mario/Scripts/PlantAnimation.js
+++ Assets/Mario/Scripts/PlantAnimation.js
@@ -5,7 +5,7 @@
 // Description: Animates a plant on trigger event
 // Instruction: Assign script to a plant (or object that has 'wiggle' as an animation label)
 */
-
+// we can check trigger event on this object rather than on player
 function Start () {									// initialize things
 	animation.Stop ();								// be sure it ani starts off stopped
 }
